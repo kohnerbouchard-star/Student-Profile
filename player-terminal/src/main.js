9c6c1039a88d65f9c962725505fa616f9a38e8fa
kohnerbouchard-star <kohnerbouchard@gmail.com>
2026-07-18 06:16:16 +0900

    feat(player): install Store transaction controller

--- player-terminal/src/main.js
+++ player-terminal/src/main.js
@@ -1,5 +1,6 @@
 import { createPlayerTerminal } from "./app.js";
 import { resolvePlayerTerminalConfig } from "./config/player-terminal.config.js";
+import { installStorePurchaseFlow } from "./features/store/store-purchase-flow.js";
 import { installFormDraftPreserver } from "./forms/form-draft-preserver.js";
 
 const mount = document.getElementById("playerTerminal");
@@ -10,8 +11,10 @@ const formDrafts = installFormDraftPreserver(mount, {
 });
 
 const terminal = createPlayerTerminal({ mount, config });
+const storePurchases = installStorePurchaseFlow({ mount, terminal, config });
 const destroyTerminal = terminal.destroy.bind(terminal);
 terminal.destroy = () => {
+  storePurchases.destroy();
   formDrafts.destroy();
   destroyTerminal();
 };
