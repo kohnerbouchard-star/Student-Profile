c92930e66c6aa9f187a0f9ac3aa10a98c66d0709
kohnerbouchard-star <kohnerbouchard@gmail.com>
2026-07-18 06:28:21 +0900

    feat(player): install Market order controller

--- player-terminal/src/main.js
+++ player-terminal/src/main.js
@@ -1,5 +1,6 @@
 import { createPlayerTerminal } from "./app.js";
 import { resolvePlayerTerminalConfig } from "./config/player-terminal.config.js";
+import { installMarketOrderFlow } from "./features/market/market-order-flow.js";
 import { installStorePurchaseFlow } from "./features/store/store-purchase-flow.js";
 import { installFormDraftPreserver } from "./forms/form-draft-preserver.js";
 
@@ -12,8 +13,10 @@ const formDrafts = installFormDraftPreserver(mount, {
 
 const terminal = createPlayerTerminal({ mount, config });
 const storePurchases = installStorePurchaseFlow({ mount, terminal, config });
+const marketOrders = installMarketOrderFlow({ mount, terminal, config });
 const destroyTerminal = terminal.destroy.bind(terminal);
 terminal.destroy = () => {
+  marketOrders.destroy();
   storePurchases.destroy();
   formDrafts.destroy();
   destroyTerminal();
