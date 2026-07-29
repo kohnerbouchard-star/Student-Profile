9888e9cf2de6d9d73c750b90fd9fe282b88fd51d
kohnerbouchard-star <kohnerbouchard@gmail.com>
2026-07-29 18:52:25 +0900

    Derive Admin proxy path from request URL

--- api/admin/[...path].js
+++ api/admin/[...path].js
@@ -1,7 +1,13 @@
 "use strict";
 
 const { proxyAdminBff } = require("../_admin-bff-proxy.js");
+const { canonicalCatchAllPath } = require("../_canonical-bff-path.js");
 
 module.exports = async function adminApiRoute(request, response) {
-  return proxyAdminBff(request, response, { proxyAdmin: true });
+  const normalizedRequest = Object.create(request);
+  normalizedRequest.query = {
+    ...(request.query && typeof request.query === "object" ? request.query : {}),
+    path: canonicalCatchAllPath(request.url, "/api/admin")
+  };
+  return proxyAdminBff(normalizedRequest, response, { proxyAdmin: true });
 };
