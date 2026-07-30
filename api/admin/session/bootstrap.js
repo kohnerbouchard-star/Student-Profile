f93954384ae6e0a77d49f7844d737c3f63905a17
kohnerbouchard-star <kohnerbouchard@gmail.com>
2026-07-30 16:13:54 +0900

    Expose explicit Admin session bootstrap route

--- api/admin/session/bootstrap.js
+++ api/admin/session/bootstrap.js
@@ -1,1 +1,12 @@
-placeholder
+"use strict";
+
+const { proxyAdminBff } = require("../../_admin-bff-proxy.js");
+
+module.exports = async function adminSessionBootstrapRoute(request, response) {
+  const normalizedRequest = Object.create(request);
+  normalizedRequest.query = {
+    ...(request.query && typeof request.query === "object" ? request.query : {}),
+    path: ["session", "bootstrap"]
+  };
+  return proxyAdminBff(normalizedRequest, response, { proxyAdmin: true });
+};
