5c20c307e75f2f68c19c1cf729841d504958500f
kohnerbouchard-star <kohnerbouchard@gmail.com>
2026-07-31 18:36:52 +0900

    test: materialize browser-only runtime fixture

---
+++ runtime-config.env.js
@@ -0,0 +1,6 @@
+window.__ECONOVARIA_RUNTIME_CONFIG__ = Object.freeze({
+  environment: "development",
+  projectRef: "runtimefixture123456",
+  supabaseUrl: "https://runtimefixture123456.supabase.co",
+  supabasePublishableKey: "sb_publishable_runtime_fixture_1234567890"
+});
