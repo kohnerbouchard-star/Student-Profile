ce31cb80a55b880f13e04a8d9d68d59096ec1c83
kohnerbouchard-star <kohnerbouchard@gmail.com>
2026-08-01 17:20:01 +0900

    fix(player): normalize Player session route paths

--- api/player-session/[...path].js
+++ api/player-session/[...path].js
@@ -1,7 +1,12 @@
 "use strict";
 
 const { proxyPlayerBff } = require("../_player-bff-proxy.js");
+const { canonicalCatchAllPath } = require("../_canonical-bff-path.js");
 
-module.exports = async function playerSessionRoute(request, response) {
-  return proxyPlayerBff(request, response, { proxyPlayer: false });
-}
+module.exports = function playerSessionRoute(request, response) {
+  const normalizedRequest = Object.create(request);
+  normalizedRequest.query = {
+    path: canonicalCatchAllPath(request.url, "/api/player-session")
+  };
+  return proxyPlayerBff(normalizedRequest, response, { proxyPlayer: false });
+};
