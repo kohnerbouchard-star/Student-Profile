6e2166885a01db3eb5ec2f570b6323cc73abe35a
kohnerbouchard-star <kohnerbouchard@gmail.com>
2026-08-01 17:20:09 +0900

    fix(player): normalize Player proxy route paths

--- api/player/[...path].js
+++ api/player/[...path].js
@@ -1,7 +1,12 @@
 "use strict";
 
 const { proxyPlayerBff } = require("../_player-bff-proxy.js");
+const { canonicalCatchAllPath } = require("../_canonical-bff-path.js");
 
-module.exports = async function playerRoute(request, response) {
-  return proxyPlayerBff(request, response, { proxyPlayer: true });
-}
+module.exports = function playerRoute(request, response) {
+  const normalizedRequest = Object.create(request);
+  normalizedRequest.query = {
+    path: canonicalCatchAllPath(request.url, "/api/player")
+  };
+  return proxyPlayerBff(normalizedRequest, response, { proxyPlayer: true });
+};
